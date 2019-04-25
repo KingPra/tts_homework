5709ea81df9f0fac81811365ddddda49624f3b1d
King Pra <kingi.pra@gmail.com>
2019-04-24 22:26:05 -0500

    playTournament has bug on return

--- functions/app.js
+++ functions/app.js
@@ -68,6 +68,8 @@ function Player (name, score) {
 
 const player1 = Player('Player 1', 0);
 const player2 = Player('Player 2', 0);
+const player3 = Player('Player 3', 0);
+const player4 = Player('Player 4', 0);
 // Define a function called playRound() that
 // Takes two player objects as arguments
 // Gets hands from each
@@ -80,11 +82,12 @@ function playRound(p1,p2) {
    let hand1 = player1.hand = getHand();
    let hand2 = player2.hand = getHand();
 
-    let winner = '';
 
-    if (hand1 === hand2) {
-        console.log(`${hand1}, ${hand2}, It's a tie!`);
-        winner = null;
+   let winner = '';
+
+   if (hand1 === hand2) {
+    console.log(`${hand1}, ${hand2}, It's a tie!`);
+    winner = null;
         // rock > scissors
         // paper > rock
         // scissors > rock
@@ -132,22 +135,17 @@ function playRound(p1,p2) {
 // Play a game to 5 wins
 
 function playGame(player1, player2, playUntil) {
-    let p1 = player1.score;
-    let p2 = player2.score;
     let tie = 0;
     let winner = '';
 
     while (player1.score <playUntil && player2.score < playUntil || tie > 20) {
         tie++
-        console.log('tie = ', tie);
-        console.log('this is p1 in playGame func: ', player1);
-        let winner = playRound(player1, player2);
-        console.log(winner);
+        winner = playRound(player1, player2);
     }
     return winner;
 }
 
-playGame(player1, player2, 5);
+//playGame(player1, player2, 5);
 // Bonus Questions
 // Define a function called playTournament():
 
@@ -156,16 +154,115 @@ playGame(player1, player2, 5);
 // Play a game between the winners of the first round.
 // Announce the tournament winner's name "[name] is the world champion";
 
+function playTournament(player1, player2, player3, player4, playUntil) {
+    let winner1 = '';
+    let winner2 = '';
+    let champ = '';
+    let tie = 0;
+// runs game for player1 and player 2
+    while (player1.score <playUntil && player2.score < playUntil || tie > 20) {
+    tie++
+    winner1 = playGame(player1, player2, playUntil);
+    }
+// reset tie to 0
+    tie = 0;
+// runs game for player 3 and player 4
+    while (player3.score <playUntil && player4.score < playUntil || tie > 20) {
+    tie++
+    winner2 = playGame(player3, player4, playUntil);
+    }
+//reset tie, winner1 score and winner2 score to 0
+    tie = 0;
+    winner1.score = 0;
+    winner2.score = 0;
+// run game for winner of game one and winner of game 2 for championship
+    while (winner1.score <playUntil && winner2.score < playUntil || tie > 20) {
+    tie++
+
+    champ = playGame(winner1, winner2, playUntil);
+    }
+    console.log('cchamp mode', champ);
+    return champ;
+
+}
+
+playTournament(player1, player2, player3, player4, 5);
 
 
 
 
 
 
+//travis code
+
+// Create a Rock, Paper, Scissors Game//
 
 
+// var game = "Let's play a game!"
+// var userName = prompt("What is your name?");
+// console.log('uersname is: ', userName);
+// // Used a method that allows for the computer to choose
+// var computerInput = Math.ceil(Math.random() * 3);
+// // var userAnswer = prompt("Rock", "Paper", "Scissors")
+
+// // Created a string that responds to the user input
+// prompt("What's up, "+ name + "?" + " " + game)
+
+// // Created responsive interaction between computer and user.
+// var userAnswer = prompt("Rock, Paper, Scissors");
+
+// // User repsonse converted to lowercase
+//  userAnswer = userAnswer.toLowerCase();
+
+// // created a variable with a choice response for the computer
+// var computerResponse = ['paper', 'rock', 'scissors'];
+
+// // used a method .indexOf that was listed on our class slides 
+// // however we never spoke about it in detail.
+
+// var usernameIndex = computerResponse.indexOf(userName);
+
+// // utilized another method that we learned in class
+// var randomNum = Math.floor(Math.random() * 3);
+// var computerChoice = computerResponse[randomNum];
+
+// // Display the choices of both the user and the computer
+// console.log(`Your choice is ${userAnswer}, the computer's choice is ${computerChoice}.`);
+
+// // This was tricky however based on what we've learned about arrays in class
+// // There had to be an easiser more efficient way to iterate through the possibilties
+// // besides using if else statements and switch cases with seem to not refelect
+// // DRY CODE
+
+// // This variable creates 3 arrays within an array with 1 statement
+// var getHand= new Array(3)
+// // A for loop that everyone is familiar with
+// for (i=0; i <3; i++)
+//     getHand [i]=new Array(3)
+
+// // these are the possibilities of the outcome of the game.I just listed 
+// // in sequential order.
+
+// getHand[0][0]="Rock"
+// getHand[0][1]="Rock"
+// getHand[0][2]="Rock"
+// getHand[1][0]="Paper"
+// getHand[1][1]="Paper"
+// getHand[1][2]="Paper"
+// getHand[2][0]="Scissors"
+// getHand[2][1]="Scissors"
+// getHand[2][2]="Scissors"
 
 
+// // I remember a colleaque in class speaking about maps
+// var resultOptions = {
+// [0]: "Tie",
+// [1]: "You win",
+// [3]: "You lose"
+// };
+
+// console.log(resultOptions);
+// // var userResult = resultOptions[randomNum][userNameIndex];
 
 
 
