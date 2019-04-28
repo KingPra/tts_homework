a35aeb29478d1dcdc310623f20a5d58f54e2d713
King Pra <kingi.pra@gmail.com>
2019-04-28 15:02:03 -0500

    fixed Player prototype syntax

--- paperRockScissors/app.js
+++ paperRockScissors/app.js
@@ -4,16 +4,14 @@ const score2 = document.querySelector("#score2");
 const hands = ["rock", "paper", "scissors"];
 let handPicked;
 
-// player class, check syntax cause it's probably currently wrong;
-function Player(name, score) {
-  return {
-    name: name,
-    score: score
-  };
+// player prototype
+function Player(name, score, hand) {
+  this.name = name;
+  this.score = score;
+  this.hand = hand;
 }
-let player1 = Player("Player1", 0);
-let player2 = Player("Player2", 0);
-
+let player1 = new Player("Player1", 0);
+let player2 = new Player("Player2", 0);
 // gets called by html buttons, returns the value of the button
 // and starts a game.
 const buttonValue = val => {
@@ -23,14 +21,17 @@ const buttonValue = val => {
 // this is a score counter;
 const reset = (p1, p2) => {
   const ending = "won the tournament. Prepare for a new game.";
-  p1.score > p2.score
+  // this logic checks for a winner and displays the winner
+  p1.score === p2.score
+    ? "The score it tied!!"
+    : p1.score > p2.score
     ? (output.textContent = `${p1.name} scored: ${p1.score}, ${
         p2.name
       } scored: ${p2.score}. ${p1.name} ${ending}`)
     : `${p2.name} scored: ${p2.score}, ${p1.name} scored: ${p1.score}. ${
         p2.name
       } ${ending}`;
-
+  // this resets all scores to 0;
   score1.textContent = p1.score = 0;
   score2.textContent = p2.score = 0;
 };
@@ -73,13 +74,6 @@ const winningHand = (p1, p2) => {
     p2.score++;
     loser = p1;
   }
-  // p1.hand === p2.hand
-  //   ? ((winner = tie), (loser = tie))
-  //   : (p1.hand === "rock" && p2.hand === "scissor") ||
-  //     (p1.hand === "scissors" && p2.hand === "paper") ||
-  //     (p1.hand === "paper" && p2.hand === "rock")
-  //   ? (((winner = p1), p1.score++), (loser = p2))
-  //   : (((winner = p2), p2.score++), (loser = p1));
 };
 
 const playGame = (p1, p2) => {
